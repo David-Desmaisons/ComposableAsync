6899a0e3165d28e2bb682344acba89fa57ed7adb
David Desmaisons <desmaisons_david@hotmail.com>
2015-11-01 14:51:44 -0200

    Add BalancingOption documentation

--- EasyActor/BalancingOption.cs
+++ EasyActor/BalancingOption.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace EasyActor
 {
+    /// <summary>
+    ///  Describe the strategy used by the <see cref="ILoadBalancerFactory"/> to create actors
+    /// </summary>
     public enum BalancingOption
     {
+        /// <summary>
+        /// <see cref="ILoadBalancerFactory"/> prefers actor creation to maximize parrelelism
+        /// </summary>
         PreferParralelism,
 
+        /// <summary>
+        /// <see cref="ILoadBalancerFactory"/> minizes object creations
+        /// </summary>
         MinizeObjectCreation
     }
 }
