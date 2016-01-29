0362e835f4c5d129df7e29a7470d84df2066d9c0
David Desmaisons <desmaisons_david@hotmail.com>
2016-01-29 20:53:08 -0200

    circular reference

---
+++ EasyActor.Test/TestInfra/DummyClass/DummyCircularReferenceClass.cs
@@ -0,0 +1,24 @@
+﻿using EasyActor.TaskHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyActor.Test.TestInfra.DummyClass
+{
+    public class DummyCircularReferenceClass : IDummyInterface3
+    {
+        public IDummyInterface3 DummyInterface { get; set; }
+
+        public async Task<int> DoAsync(bool First)
+        {
+            if (First == false)
+                return 1;
+
+            var res = DummyInterface.DoAsync(false).Result + 1;
+            res += await DummyInterface.DoAsync(false);
+            return res;
+        }
+    }
+}
