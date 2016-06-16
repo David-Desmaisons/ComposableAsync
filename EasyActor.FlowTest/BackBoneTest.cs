4649afad0ca4fe1b3710e04bc7ba1edcf2f2a283
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-06-16 18:29:35 -0300

    Improving tests

--- EasyActor.FlowTest/BackBoneTest.cs
+++ EasyActor.FlowTest/BackBoneTest.cs
@@ -10,7 +10,6 @@ using EasyActor.Flow;
 using System.Reactive.Linq;
 using System.Reactive.Disposables;
 using Xunit;
-using System.Reactive.Linq;
 
 namespace EasyActor.FlowTest
 {
@@ -19,7 +18,8 @@ namespace EasyActor.FlowTest
     {
         private IBackbone<bool, int> _BackBone;
         private IDictionary<Type, object> _Processors = new Dictionary<Type, object>();
-        private IProcessor<bool, string, int> _Processor;
+        private IProcessor<bool, string, int> _StringProcessor;
+        private IProcessor<bool, int, int> _IntProcessor;
         private IProgress<int> _Progess;
         private CancellationToken _CancellationToken;
         private IObservable<string> _SingleValue;
@@ -28,12 +28,14 @@ namespace EasyActor.FlowTest
         public BackBoneTest()
         {
             _CancellationToken = new CancellationToken();
-            _Processor = Substitute.For<IProcessor<bool, string, int>>();
+            _StringProcessor = Substitute.For<IProcessor<bool, string, int>>();
+            _IntProcessor = Substitute.For<IProcessor<bool, int, int>>();
             _Progess = Substitute.For<IProgress<int>>();
             _ObservableHelper = new ObservableHelper();
             _Processors = new Dictionary<Type, object>()
             {
-                { typeof(string), _Processor }
+                { typeof(string), _StringProcessor },
+                { typeof(int), _IntProcessor }
             };
             _BackBone = new BackBone<bool, int>(_Processors);
             _SingleValue = Observable.Return("Value");
@@ -55,14 +57,14 @@ namespace EasyActor.FlowTest
         public async Task Process_WithMessageTypeKnown_CallProcessor()
         {
             await _BackBone.Process("test", _Progess, _CancellationToken);
-            await _Processor.Received(1).Process("test", _BackBone, _Progess, Arg.Any<CancellationToken>());
+            await _StringProcessor.Received(1).Process("test", _BackBone, _Progess, Arg.Any<CancellationToken>());
         }
 
         [Fact]
         public async Task Process_CalledWithANullReferenceProgess_ProvidesANullProgress()
         {
             await _BackBone.Process("test", null, _CancellationToken);
-            await _Processor.Received(1).Process("test", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
+            await _StringProcessor.Received(1).Process("test", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
         }
 
         [Fact]
@@ -87,7 +89,7 @@ namespace EasyActor.FlowTest
             _BackBone.Connect(_ObservableHelper.GetObservable());
             _ObservableHelper.Observe("Banana");
 
-            await _Processor.Received(1).Process("Banana", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
+            await _StringProcessor.Received(1).Process("Banana", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
         }
 
         [Fact]
@@ -96,7 +98,7 @@ namespace EasyActor.FlowTest
             using (_BackBone.Connect(_ObservableHelper.GetObservable())) { }
             _ObservableHelper.Observe("Banana");
 
-            await _Processor.DidNotReceive().Process("Banana", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
+            await _StringProcessor.DidNotReceive().Process("Banana", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
         }
 
         [Fact]
@@ -106,7 +108,7 @@ namespace EasyActor.FlowTest
             _BackBone.Dispose();
             _ObservableHelper.Observe("Banana");
 
-            await _Processor.DidNotReceive().Process("Banana", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
+            await _StringProcessor.DidNotReceive().Process("Banana", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
         }
 
         [Fact]
@@ -116,19 +118,32 @@ namespace EasyActor.FlowTest
             var disp =  _BackBone.Connect(_SingleValue);
 
             disp.Should().Be(Disposable.Empty);
-            await _Processor.DidNotReceive().Process("Value", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
+            await _StringProcessor.DidNotReceive().Process("Value", _BackBone, Arg.Any<NullProgess<int>>(), Arg.Any<CancellationToken>());
         }
 
 
         [Fact]
-        public async Task GetObservable_SendMessageAsExpected() {
-            var observed = new List<string>();
-            var obs = _BackBone.GetObservable<string>();
-            obs.Subscribe(s => observed.Add(s));
-
+        public async Task GetObservable_SendMessageAsExpected_Result()
+        {
+            var observed = SetUpObservable<string>();
             await _BackBone.Process("string", null, CancellationToken.None);
-
             observed.Should().BeEquivalentTo("string");
+        }
+
+        [Fact]
+        public async Task GetObservable_SendMessageAsExpected_Empty() 
+        {
+            var observed = SetUpObservable<string>();
+            await _BackBone.Process(23, null, CancellationToken.None);
+            observed.Should().BeEmpty();
+        }
+
+        private List<T> SetUpObservable<T>() 
+        {
+            var observed = new List<T>();
+            var obs = _BackBone.GetObservable<T>();
+            obs.Subscribe(s => observed.Add(s));
+            return observed;
         }
     }
 }
