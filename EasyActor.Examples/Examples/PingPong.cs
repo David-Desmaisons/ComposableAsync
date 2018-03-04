cd93709025d3a17a5ad9a65ed7c37bbb95923b3d
desmaisons_david <desmaisons_david@hotmail.com>
2018-03-04 20:13:18 -0300

    Clean-up

--- EasyActor.Examples/Examples/PingPong.cs
+++ EasyActor.Examples/Examples/PingPong.cs
@@ -3,20 +3,20 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
- 
+
 using EasyActor.TaskHelper;
 using Xunit;
 
 namespace EasyActor.Examples
 {
-     
+
     public class PingPong
     {
         public PingPong() : this(ThreadPriority.Normal)
         {
         }
 
-        private ThreadPriority _Priority;
+        private readonly ThreadPriority _Priority;
         internal PingPong(ThreadPriority priority)
         {
             _Priority = priority;
@@ -25,19 +25,19 @@ namespace EasyActor.Examples
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public async Task Test(bool TaskPool)
+        public async Task Test(bool taskPool)
         {
-            IActorFactory fact = new ActorFactory(t=>t.Priority=_Priority);
+            IActorFactory fact = new ActorFactory(t => t.Priority = _Priority);
 
-            var One = new PingPonger("Bjorg");
-           
-            IPingPonger Actor1 = fact.Build<IPingPonger>(One);
+            var one = new PingPonger("Bjorg");
 
-            var Two = new PingPonger("Lendl"); 
-            var fact2 = TaskPool ?  new TaskPoolActorFactory() : fact;
+            IPingPonger Actor1 = fact.Build<IPingPonger>(one);
+
+            var Two = new PingPonger("Lendl");
+            var fact2 = taskPool ? new TaskPoolActorFactory() : fact;
             IPingPonger Actor2 = fact2.Build<IPingPonger>(Two);
 
-            One.Ponger = Actor2;
+            one.Ponger = Actor2;
             Two.Ponger = Actor1;
 
             var watch = new Stopwatch();
@@ -52,9 +52,10 @@ namespace EasyActor.Examples
 
             watch.Stop();
 
-            Console.WriteLine("Total Ping:{0} Total Time: {1}", One.Count, watch.Elapsed);
-            Console.WriteLine(One.Count);
+            Console.WriteLine($"Total Ping:{one.Count} Total Time: {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine(one.Count);
             Console.WriteLine(Two.Count);
+            Console.WriteLine($"Ping/ms:{one.Count/watch.ElapsedMilliseconds}");
         }
     }
 }
--- EasyActor.Examples/PingPongConsole/Program.cs
+++ EasyActor.Examples/PingPongConsole/Program.cs
@@ -1,6 +1,5 @@
 ﻿using EasyActor.Examples;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace PingPongConsole
