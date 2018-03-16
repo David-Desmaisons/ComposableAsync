d4d33756cb67cfc5abba26bced9aea244958bd77
WAY2\david.desmaisons <david.desmaisons@way2.com.br>
2018-03-16 18:38:55 -0300

    Caching result for fiber proxy

---
+++ EasyActor/Proxy/FiberBehaviourCacherDispatcher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using EasyActor.TaskHelper;
+
+namespace EasyActor.Proxy {
+    internal static class FiberBehaviourCacherDispatcher<T> 
+    {
+        private static readonly MethodInfo _Proceed = typeof(FiberBehaviourCacherDispatcher<T>).GetMethod(nameof(Proceed), BindingFlags.Static | BindingFlags.NonPublic);
+        private static readonly Dictionary<MethodInfo, Func<IFiber, IInvocation, object>> _Cache = new Dictionary<MethodInfo, Func<IFiber, IInvocation, object>>();
+
+        static FiberBehaviourCacherDispatcher() 
+        {
+            var type = typeof(T);
+            RegisterType(type);
+            foreach (var @interface in type.GetInterfaces()) 
+            {
+                RegisterType(@interface);
+            }
+        }
+
+        private static void RegisterType(Type t) 
+        {
+            foreach(var methodInfo in t.GetMethods()) 
+            {
+                _Cache.Add(methodInfo, BuildTransformFunction(methodInfo));
+            }
+        }
+
+        public static Func<IFiber, IInvocation, object> GetTransformFunction(IInvocation invocation)
+        {
+            Func<IFiber, IInvocation, object> res = null;
+            _Cache.TryGetValue(invocation.Method, out res);
+            return res;  
+        }
+
+        private static Func<IFiber, IInvocation, object> BuildTransformFunction(MethodInfo method) 
+        {
+            Func<IFiber, IInvocation, object> res = null;
+            var td = method.ReturnType.GetTaskType();
+            switch (td.MethodType)
+            {
+                case TaskType.Void:
+                    res = (fiber, invocation) => 
+                    {
+                        fiber.Dispatch(invocation.Call);
+                        return null;
+                    };
+                    break;
+
+                case TaskType.Task:
+                    res = (fiber, invocation) => fiber.Enqueue(invocation.Call<Task>);
+                    break;
+
+                case TaskType.GenericTask:
+                    var mi = _Proceed.MakeGenericMethod(td.Type);
+                    res = (fiber, invocation) => mi.Invoke(null, new object[] { fiber, invocation });
+                    break;
+
+                case TaskType.None:
+                    throw new NotSupportedException("Actor method should only return Task, Task<T> or void");
+            }
+
+            return res;
+        }
+
+        private static object Proceed<TResult>(IFiber fiber, IInvocation invocation)
+        {
+            return fiber.Enqueue(invocation.Call<Task<TResult>>);
+        }
+    }
+}
--- EasyActor/Proxy/FiberDispatcherInterceptor.cs
+++ EasyActor/Proxy/FiberDispatcherInterceptor.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Reflection;
-using System.Threading.Tasks;
-using Castle.DynamicProxy;
-using EasyActor.TaskHelper;
+﻿using Castle.DynamicProxy;
 
-namespace EasyActor.Proxy
+namespace EasyActor.Proxy 
 {
     internal class FiberDispatcherInterceptor<T> : IInterceptor
     {
-        private static readonly MethodInfo _Proceed = typeof(FiberDispatcherInterceptor<T>).GetMethod(nameof(Proceed), BindingFlags.Instance | BindingFlags.NonPublic);
-        private static readonly Type _Type = typeof(T);
-
         private readonly IFiber _Fiber;
 
         public FiberDispatcherInterceptor(IFiber fiber)
@@ -20,38 +13,14 @@ namespace EasyActor.Proxy
 
         public void Intercept(IInvocation invocation)
         {
-            var method = invocation.Method;
-            if (!method.DeclaringType.IsAssignableFrom(_Type))
+            var function = FiberBehaviourCacherDispatcher<T>.GetTransformFunction(invocation);
+            if (function==null)
             {
                 invocation.Proceed();
                 return;
             }
 
-            var td = method.ReturnType.GetTaskType();
-            switch (td.MethodType)
-            {
-                case TaskType.Void:
-                    _Fiber.Dispatch(invocation.Call);
-                    invocation.ReturnValue = null;
-                    break;
-
-                case TaskType.Task:
-                    invocation.ReturnValue = _Fiber.Enqueue(invocation.Call<Task>);
-                    break;
-
-                case TaskType.GenericTask:
-                    var mi = _Proceed.MakeGenericMethod(td.Type);
-                    mi.Invoke(this, new object[] { invocation });
-                    break;
-
-                case TaskType.None:
-                    throw new NotSupportedException("Actor method should only return Task, Task<T> or void");
-            }
-        }
-
-        private void Proceed<TResult>(IInvocation invocation)
-        {
-            invocation.ReturnValue = _Fiber.Enqueue(invocation.Call<Task<TResult>>);
+            invocation.ReturnValue = function(_Fiber, invocation);
         }
     }
 }
