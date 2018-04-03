e742615ab7dc90a7d6cc4a0f7d772a39e97c878d
desmaisons_david <desmaisons_david@hotmail.com>
2018-04-03 00:33:51 -0300

    Improve test

--- Concurrent.Test/Dispatchers/RateLimiterDispatcherTests.cs
+++ Concurrent.Test/Dispatchers/RateLimiterDispatcherTests.cs
@@ -14,18 +14,18 @@ namespace Concurrent.Test.Dispatchers
     {
         private readonly RateLimiterDispatcher _RateLimiterDispatcher;
         private readonly ITestOutputHelper _TestOutput;
+        private const int Interval = 200;
 
         public RateLimiterDispatcherTests(ITestOutputHelper testOutput)
         {
             _TestOutput = testOutput;
-            var limiter = new CountByIntervalAwaitableConstraint(1, TimeSpan.FromMilliseconds(200));
+            var limiter = new CountByIntervalAwaitableConstraint(1, TimeSpan.FromMilliseconds(Interval));
             _RateLimiterDispatcher = new RateLimiterDispatcher(limiter);
         }
 
         [Fact]
         public async Task Dispatcher_Limit_Number_Of_Call()
         {
-
             var count = 0;
             var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(700));
             var stopWatch = Stopwatch.StartNew();
@@ -42,9 +42,11 @@ namespace Concurrent.Test.Dispatchers
             }
             stopWatch.Stop();
 
+            var numberOfCall = (int) (Math.Truncate((decimal)stopWatch.ElapsedMilliseconds / Interval));
+
             _TestOutput.WriteLine($"Ended: {DateTime.Now:O}");
             _TestOutput.WriteLine($"Count:{count}, Time spend: {stopWatch.Elapsed}");
-            count.Should().Be(4);
+            count.Should().Be(numberOfCall);
         }
     }
 }
