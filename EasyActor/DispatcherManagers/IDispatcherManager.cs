b8e4573c134f70dae72015595cab4719755f1a8c
David <desmaisons_david@hotmail.com>
2019-03-13 22:42:28 -0300

    Add documentation

--- EasyActor/DispatcherManagers/IDispatcherManager.cs
+++ EasyActor/DispatcherManagers/IDispatcherManager.cs
@@ -4,12 +4,25 @@ using EasyActor.Options;
 
 namespace EasyActor.DispatcherManagers
 {
+    /// <summary>
+    /// Dispatcher manager
+    /// </summary>
     public interface IDispatcherManager : IAsyncDisposable
     {
+        /// <summary>
+        /// true if the Dispatcher should be released
+        /// </summary>
         bool DisposeDispatcher { get; }
 
+        /// <summary>
+        /// Actor factory typr
+        /// </summary>
         ActorFactorType Type { get; }
 
+        /// <summary>
+        /// Returns a consumable Dispatcher
+        /// </summary>
+        /// <returns></returns>
         ICancellableDispatcher GetDispatcher();
     }
 }
