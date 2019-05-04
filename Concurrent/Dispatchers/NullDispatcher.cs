2050279ad2a3a15db685abcd3738871ceeb7b42f
David <desmaisons_david@hotmail.com>
2019-05-04 15:44:19 -0300

    Adding tests

--- Concurrent/Dispatchers/NullDispatcher.cs
+++ Concurrent/Dispatchers/NullDispatcher.cs
@@ -15,27 +15,32 @@ namespace Concurrent.Dispatchers
         /// </summary>
         public static IDispatcher Instance { get; } = new NullDispatcher();
 
+        /// <inheritdoc />
         public void Dispatch(Action action)
         {
             action();
         }
 
+        /// <inheritdoc />
         public Task Enqueue(Action action)
         {
             action();
             return Task.CompletedTask;
         }
 
+        /// <inheritdoc />
         public Task<T> Enqueue<T>(Func<T> action)
         {
             return Task.FromResult(action());
         }
 
+        /// <inheritdoc />
         public async Task Enqueue(Func<Task> action)
         {
             await action();
         }
 
+        /// <inheritdoc />
         public async Task<T> Enqueue<T>(Func<Task<T>> action)
         {
             return await action();
