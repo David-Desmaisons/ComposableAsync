9874e9ea9bbac18ced183eef8da0115300af5d30
David <desmaisons_david@hotmail.com>
2019-05-19 19:06:42 -0300

    Adding documentation

--- ComposableAsync.Core/DispatcherManager/MonoDispatcherManager.cs
+++ ComposableAsync.Core/DispatcherManager/MonoDispatcherManager.cs
@@ -3,22 +3,35 @@ using System.Threading.Tasks;
 
 namespace ComposableAsync
 {
+    /// <summary>
+    /// <see cref="IDispatcherManager"/> implementation based on single <see cref="ICancellableDispatcher"/>
+    /// </summary>
     public sealed class MonoDispatcherManager : IDispatcherManager
     {
+        /// <inheritdoc cref="IDispatcherManager"/>
         public bool DisposeDispatcher { get; }
+
+        /// <inheritdoc cref="IDispatcherManager"/>
         public ICancellableDispatcher GetDispatcher() => _Dispatcher;
-        public Task DisposeAsync()
-        {
-            return DisposeDispatcher && (_Dispatcher is IAsyncDisposable disposable) ?
-                disposable.DisposeAsync() : Task.CompletedTask;
-        }
 
         private readonly ICancellableDispatcher _Dispatcher;
 
+        /// <summary>
+        /// Create 
+        /// </summary>
+        /// <param name="dispatcher"></param>
+        /// <param name="shouldDispose"></param>
         public MonoDispatcherManager(ICancellableDispatcher dispatcher, bool shouldDispose = false)
         {
             _Dispatcher = dispatcher;
             DisposeDispatcher = shouldDispose;
+        }
+
+        /// <inheritdoc cref="IDispatcherManager"/>
+        public Task DisposeAsync()
+        {
+            return DisposeDispatcher && (_Dispatcher is IAsyncDisposable disposable) ?
+                disposable.DisposeAsync() : Task.CompletedTask;
         }
     }
 }
