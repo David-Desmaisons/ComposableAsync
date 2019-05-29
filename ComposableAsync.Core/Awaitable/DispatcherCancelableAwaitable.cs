5a129501a8c6f9b0682dd6575706c29042a9c388
David <desmaisons_david@hotmail.com>
2019-05-29 19:13:33 -0300

    wip cancellable await

---
+++ ComposableAsync.Core/Awaitable/DispatcherCancelableAwaitable.cs
@@ -0,0 +1,13 @@
+﻿namespace ComposableAsync.Awaitable
+{
+    public struct DispatcherCancelableAwaitable
+    {
+        private readonly DispatcherCancelableAwaiter _DispatcherCancelableAwaiter;
+        public DispatcherCancelableAwaitable(DispatcherCancelableAwaiter dispatcherCancelableAwaiter)
+        {
+            _DispatcherCancelableAwaiter = dispatcherCancelableAwaiter;
+        }
+
+        public DispatcherCancelableAwaiter GetAwaiter() => _DispatcherCancelableAwaiter;
+    }
+}
--- ComposableAsync.Core/DispatcherExtension.cs
+++ ComposableAsync.Core/DispatcherExtension.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security;
+using System.Threading;
+using ComposableAsync.Awaitable;
 
 namespace ComposableAsync
 {
@@ -12,43 +14,6 @@ namespace ComposableAsync
     public static class DispatcherExtension
     {
         /// <summary>
-        /// Dispatcher awaiter, making a dispatcher awaitable
-        /// </summary>
-        public struct DispatcherAwaiter : INotifyCompletion
-        {
-            /// <summary>
-            /// Dispatcher never is synchronous
-            /// </summary>
-            public bool IsCompleted => false;
-
-            private readonly IDispatcher _Dispatcher;
-
-            /// <summary>
-            /// Construct a NotifyCompletion fom a dispatcher
-            /// </summary>
-            /// <param name="dispatcher"></param>
-            public DispatcherAwaiter(IDispatcher dispatcher)
-            {
-                _Dispatcher = dispatcher;
-            }
-
-            /// <summary>
-            /// Dispatch on complete
-            /// </summary>
-            /// <param name="continuation"></param>
-            [SecuritySafeCritical]
-            public void OnCompleted(Action continuation)
-            {
-                _Dispatcher.Dispatch(continuation);
-            }
-
-            /// <summary>
-            /// No Result
-            /// </summary>
-            public void GetResult() { }
-        }
-
-        /// <summary>
         /// Returns awaitable to enter in the dispatcher context
         /// This extension method make a dispatcher awaitable
         /// </summary>
@@ -57,6 +22,18 @@ namespace ComposableAsync
         public static DispatcherAwaiter GetAwaiter(this IDispatcher dispatcher)
         {
             return new DispatcherAwaiter(dispatcher);
+        }
+
+        /// <summary>
+        /// Return a cancellable awaitable linked to the corresponding ICancellableDispatcher
+        /// </summary>
+        /// <param name="dispatcher"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Corresponding awaitable struct</returns>
+        public static DispatcherCancelableAwaitable ConfigureAwait(this ICancellableDispatcher dispatcher, CancellationToken cancellationToken)
+        {
+            var awaiter = new DispatcherCancelableAwaiter(dispatcher, cancellationToken);
+            return new DispatcherCancelableAwaitable(awaiter);
         }
 
         /// <summary>
