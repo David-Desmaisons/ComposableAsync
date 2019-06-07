695fbfc3bedf3c1efa2c94b4985c2afee79f7215
David <desmaisons_david@hotmail.com>
2019-06-07 05:59:50 -0300

    Adding sugar API

---
+++ ComposableAsync.Factory/CancellableDispatcherExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace ComposableAsync.Factory
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class CancellableDispatcherExtension
+    {
+        /// <summary>
+        /// Proxify a POCO usando the <see cref="ICancellableDispatcher"/> as an ansynchroneous transformer
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="concrete">Object to be proxified</param>
+        /// <returns></returns>
+        public static T Proxify<T>(ICancellableDispatcher @this, T concrete) where T : class
+        {
+            var factory = new ProxyFactory(@this);
+            return factory.Build<T>(concrete);
+        }
+    }
+}
