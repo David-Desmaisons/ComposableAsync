c36728f14ab51e00c502bffa12381abee273c9d1
David <desmaisons_david@hotmail.com>
2019-06-09 19:36:30 -0300

    Adding by user policy

--- ComposableAsync.Asp/DispatcherExtension.cs
+++ ComposableAsync.Asp/DispatcherExtension.cs
@@ -3,17 +3,18 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Security.Claims;
 
 namespace ComposableAsync.Asp
 {
     public static class DispatcherExtension
     {
-        public static void UseDispatcher(IApplicationBuilder app, ICancellableDispatcher dispatcher)
+        public static void UseDispatcher(this IApplicationBuilder app, ICancellableDispatcher dispatcher)
         {
             app.Use((context, next) => dispatcher.Enqueue(next));
         }
 
-        public static void UseDispatcher(IApplicationBuilder app, Func<HttpContext,ICancellableDispatcher> dispatcherFinder)
+        public static void UseDispatcher(this IApplicationBuilder app, Func<HttpContext,ICancellableDispatcher> dispatcherFinder)
         {
             app.Use((context, next) =>
             {
@@ -22,7 +23,7 @@ namespace ComposableAsync.Asp
             });
         }
 
-        public static void UseDispatcherByRemoteIpAddress(IApplicationBuilder app, Func<ICancellableDispatcher> dispatcherFinder)
+        public static void UseDispatcherByRemoteIpAddress(this IApplicationBuilder app, Func<ICancellableDispatcher> dispatcherFinder)
         {
             var cache = new Dictionary<IPAddress, ICancellableDispatcher>();
             app.Use((context, next) =>
@@ -32,7 +33,20 @@ namespace ComposableAsync.Asp
             });
         }
 
-        private static ICancellableDispatcher GetOrCreate(IDictionary<IPAddress, ICancellableDispatcher> cache, IPAddress address, Func<ICancellableDispatcher> dispatcherFinder)
+        public static void UseDispatcherByUser(this IApplicationBuilder app, Func<ICancellableDispatcher> dispatcherFinder)
+        {
+            var cache = new Dictionary<ClaimsPrincipal, ICancellableDispatcher>();
+            app.Use((context, next) =>
+            {
+                if (context.User == null)
+                    return next();
+
+                var dispatcher = GetOrCreate(cache, context.User, dispatcherFinder);
+                return dispatcher.Enqueue(next);
+            });
+        }
+
+        private static ICancellableDispatcher GetOrCreate<T>(IDictionary<T, ICancellableDispatcher> cache, T address, Func<ICancellableDispatcher> dispatcherFinder)
         {
             if (cache.TryGetValue(address, out var dispatcher))
                 return dispatcher;
